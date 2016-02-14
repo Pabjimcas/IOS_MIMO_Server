24eb5531ca870946dcc617448cae0c389ef1f7d5
mibaldi <mibaldi2@gmail.com>
2016-02-14 17:44:11 +0100

    add list of ingredients and join ingredients id with specific task

--- app/controllers/IngredientController.java
+++ app/controllers/IngredientController.java
@@ -13,8 +13,10 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-public class IngredientController extends Controller{
-	
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class IngredientController extends Controller {
+
 	public Result newIngredient() {
 		Form<Ingredient> form = Form.form(Ingredient.class).bindFromRequest();
 		if (form.hasErrors()) {
@@ -22,78 +24,134 @@ public class IngredientController extends Controller{
 		}
 		Ingredient ingredient = form.get();
 		ingredient.save();
-		if(request().accepts("application/json")){
-    		return ok(Json.toJson(ingredient));
-    	}
-    	return badRequest("Unsupported format");
-    }
-	
+		if (request().accepts("application/json")) {
+			return ok(Json.toJson(ingredient));
+		}
+		return badRequest("Unsupported format");
+	}
+
+	public Result newIngredients() {
+		JsonNode json = request().body().asJson();
+
+		for (JsonNode i : json.withArray("Ingredients")) {
+			Ingredient ingredient = new Ingredient();
+			ingredient.name = i.get("name").asText();
+			ingredient.frozen = i.get("frozen").asBoolean();
+			ingredient.category = i.get("category").asText();
+			ingredient.baseType = i.get("baseType").asText();
+			ingredient.save();
+
+		}
+		List<Ingredient> ingredientList = Ingredient.find.all();
+
+		if (ingredientList.size() == 0) {
+			return badRequest("No se han encontrado resultados en la búsqueda");
+		}
+		if (request().accepts("application/json")) {
+			return ok(Json.toJson(ingredientList));
+		}
+		return badRequest("Unsupported format");
+	}
+
 	public Result getIngredient(Long id) {
 		Ingredient ingredient = Ingredient.findById(id);
-		if(ingredient == null){
+		if (ingredient == null) {
 			return badRequest("El ingrediente no existe");
 		}
-		if(request().accepts("application/json")){
-    		return ok(Json.toJson(ingredient));
-    	}
-    	return badRequest("Unsupported format");
-    }
-	
-	public Result getIngredients(){
-		
-		List<Ingredient> ingredientList = Ingredient.find.all();
-		
-		if(ingredientList.size() == 0){
-			return badRequest("No se han encontrado resultados en la búsqueda");
-		}else{
-			if(request().accepts("application/json")){
-	    		return ok(Json.toJson(ingredientList));
-	    	}
-	    	return badRequest("Unsupported format");
+
+		if (request().accepts("application/json")) {
+			return ok(Json.toJson(ingredient));
 		}
-    }
-	
+		return badRequest("Unsupported format");
+	}
+
+	public Result getIngredients() {
+
+		List<Ingredient> ingredientList = Ingredient.find.all();
+
+		if (ingredientList.size() == 0) {
+			return badRequest("No se han encontrado resultados en la búsqueda");
+		}
+		else {
+			if (request().accepts("application/json")) {
+				return ok(Json.toJson(ingredientList));
+			}
+			return badRequest("Unsupported format");
+		}
+	}
+
 	public Result newIngredientTask(Long idIngredient, Long idTask) {
 		Form<IngredientTask> form = Form.form(IngredientTask.class).bindFromRequest();
 		if (form.hasErrors()) {
 			return badRequest(ControllerHelper.errorJson(2, "Datos incorrectos", form.errorsAsJson()));
 		}
 		Ingredient ingredient = Ingredient.findById(idIngredient);
-		if(ingredient == null){
+		if (ingredient == null) {
 			return notFound("El ingrediente no existe");
 		}
 		Task task = Task.findById(idTask);
-		if(task == null){
+		if (task == null) {
 			return notFound("La tarea no existe");
 		}
 		IngredientTask ingredientTask = form.get();
 		ingredientTask.ingredient = ingredient;
 		ingredientTask.task = task;
 		ingredientTask.save();
-		if(request().accepts("application/json")){
-    		return ok("Ingredient assigned to task");
-    	}
-    	return badRequest("Unsupported format");
-    }
-	
-	public Result addRecipe(Long idIngredient,Long idRecipe) {
-		
+		if (request().accepts("application/json")) {
+			return ok("Ingredient assigned to task");
+		}
+		return badRequest("Unsupported format");
+	}
+
+	public Result newIngredientsTask(Long idTask) {
+
+		Task task = Task.findById(idTask);
+		if (task == null) {
+			return notFound("La tarea no existe");
+		}
+		JsonNode json = request().body().asJson();
+
+		for (JsonNode i : json.withArray("Ingredients")) {
+
+			Long id = i.get("id").asLong();
+			Ingredient ingredient = Ingredient.findById(id);
+			if (ingredient == null) {
+				return notFound("El ingrediente no existe");
+			}
+			IngredientTask ingredientTask = new IngredientTask();
+			String measure = i.get("measure").asText();
+			Float quantity = (float) i.get("quantity").asDouble();
+			ingredientTask.measure = measure;
+			ingredientTask.quantity = quantity;
+			ingredientTask.ingredient = ingredient;
+			ingredientTask.task = task;
+			ingredientTask.save();
+
+		}
+		if (request().accepts("application/json")) {
+			return ok("Ingredient assigned to task");
+		}
+		return badRequest("Unsupported format");
+	}
+
+	public Result addRecipe(Long idIngredient, Long idRecipe) {
+
 		Recipe recipe = Recipe.findById(idRecipe);
-		if(recipe == null){
+		if (recipe == null) {
 			return notFound("La receta no existe");
 		}
-		
+
 		Ingredient ingredient = Ingredient.findById(idIngredient);
-		if(ingredient == null){
+		if (ingredient == null) {
 			return notFound("El ingrediente no existe");
 		}
-		
+
 		recipe.ingredients.add(ingredient);
 		recipe.update();
-		if(request().accepts("application/json")){
-    		return ok("Ingredient assigned to recipe");
-    	}
-    	return badRequest("Unsupported format");
-    }
+		if (request().accepts("application/json")) {
+			return ok("Ingredient assigned to recipe");
+		}
+		return badRequest("Unsupported format");
+	}
 
 }
